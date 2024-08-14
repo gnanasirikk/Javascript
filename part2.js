d42092cea2a186a65ac89c1a00b6b2b5dad5893a
Gnana Siri <gnanasirikk@gmail.com>
2024-08-14 15:28:24 +0530

    part2

---
+++ part2.js
@@ -0,0 +1,18 @@
+console.log("hi");
+console.log("Gnana siri");
+let a=90
+let b=21
+//let c=("Total is:"+(a+b)+"cm");
+let c=`Total is:${a+b} cm hiuuu`
+console.log(c)
+
+//Conditional statements
+console.log("Before if stmt")
+let age=8
+    if(age>18){
+        console.log(" eligible")
+        let ah=9
+            console.log(5*ah)}
+    if(age<18){
+        console.log("under 18")}
+console.log("after")
