56b5ac49eaa465497775e0d531c00704a78cf6c4
Gnana Siri <gnanasirikk@gmail.com>
2024-08-14 20:52:44 +0530

    part3

--- part3.js
+++ part3.js
@@ -1,5 +1,47 @@
-<<<<<<< HEAD
-let a=9
-=======
+let str="         gnana siri             ";
+m=str.toUpperCase();
+mj=str.toUpperCase().trim();
+console.log(mj);
 
->>>>>>> be659f8c384bef547b28664595d7d2657abbe7ed
+console.log(m);
+let h="Yashaswini";
+p=h.indexOf("h");
+console.log(p);
+
+//PQ
+let msg="help!";
+k=msg.trim().toUpperCase();
+console.log(k);
+let name = "ApnaCollege"
+console.log(name.slice(4,9));
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna","Ours"));
+let fruits =["banana",'guava','kiwi'];
+fruits[1]='papaya';
+fruits.push("mango")
+fruits.pop();
+
+//pq
+let start=['jan','july','mar','aug'];
+final=start.shift();
+console.log(final);
+
+//PQ
+let month=["jan",'july','mar','aug'];
+k=month.splice(1);
+console.log(k);
+let lang =["c",'c++','html','js','python','java','c#','sql'];
+console.log(lang.reverse().indexOf('js'));
+
+//PQ-tic tac toe
+let tic=[['x',null,null ],[null,'o',null],[null,null,null]];
+console.log(tic);
+
+//assignment
+let first=[2,4,3,12,1];
+console.log(first);
+
+console.log(first.reverse().slice(1));
+
+
+
