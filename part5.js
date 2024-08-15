1751c04ae96a2e4858ba93ef13d9f855604acf21
Gnana Siri <gnanasirikk@gmail.com>
2024-08-15 20:05:25 +0530

    part5

---
+++ part5.js
@@ -0,0 +1,25 @@
+const twitter={
+    username : 'heartlesssoul',
+    likes : 7,
+    comments : 689,
+    posts : 81,
+};
+
+Math.floor(Math.random()*100)+1;
+
+let guess=prompt("Enter range:");
+console.log(Math.floor(Math.random(guess))+1);
+
+//ASSIGNNMENT
+Math.floor(Math.random()*6)+1;
+
+car={
+    name : "bmw",
+    model : "xy9",
+    color : "black"
+};
+console.log(car.name);
+
+car.name = "toyota";
+
+
