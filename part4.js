034e0493c49547c65cf75b1addbf4d165b1690ea
Gnana Siri <gnanasirikk@gmail.com>
2024-08-15 16:50:11 +0530

    part4

---
+++ part4.js
@@ -0,0 +1,16 @@
+//for loops
+for(let i=0;i<=100;i++)
+    console.log(i);
+
+//odd numbers
+for(i=1;i<=15;i=i+2){
+    console.log(i);
+}
+
+//even numbers
+for(i=2;i<=15;i=i+2){
+    console.log(i);
+}
+for(i=1; ;i=i+2){
+    console.log(i);
+}
